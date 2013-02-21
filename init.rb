e12e87480b4061d913f7472153d6de45d9788844
Dmitriy Brodnitskiy <dmitriy.brodnitskiy@twinslash.com>
2013-02-21 17:29:23 +0300

    refactoring

--- init.rb
+++ init.rb
@@ -13,13 +13,22 @@ end
 Redmine::WikiFormatting::Macros.register do
   desc "Insert the description of the sub tasks. Example: !{{child_issues}}"
   macro :child_issues do |obj, args|
-    p "========================================"
-    p args
-    unless args.empty? #level_param=(all|1,2,3...), subject_param=(3|none,1,2,3...), task_param
-      level_arg, subject_arg, task_arg = args[0..2]
-      level_arg  = level_arg.to_i  unless level_arg == ''
-      subject_arg = subject_arg.empty? ? 3 : subject_arg.to_i
-    end
+ #level_param=(all|1,2,3...), subject_param=(3|none,1,2,3...), task_param
+
+      level_arg, subject_arg, task_arg = args[0..2] unless args.nil? || args.empty?
+
+      if level_arg.nil? || level_arg == ''
+        level_arg = 'all'
+      else
+        level_arg = level_arg.to_i
+      end
+
+      if subject_arg.nil? || subject_arg.empty?
+        subject_arg = 3
+      else
+        subject_arg = subject_arg.to_i unless subject_arg == 'none'
+      end
+
 
     if params[:controller] == 'issues'
       content = Issue.find(obj.id).tree_child(level_arg, subject_arg, task_arg).insert(0, "<br/>")
@@ -27,9 +36,9 @@ Redmine::WikiFormatting::Macros.register do
       content = auto_link content
       case task_arg
         when "link"
-          content.gsub!(/ #(\d+)/) { |id| "#{link_to_issue(Issue.find(id.delete(' #')), :subject => false, :tracker => false)}"}.html_safe
+          content.gsub!(/ #(\d+)/) { |id| " #{link_to_issue(Issue.find(id.delete('#')), :subject => false, :tracker => false)}"}.html_safe
         when "full"
-          content.gsub!(/ #(\d+)/) { |id| "#{link_to_issue(Issue.find(id.delete(' #')), :subject => true, :tracker => true)}"}.html_safe
+          content.gsub!(/ #(\d+)/) { |id| " #{link_to_issue(Issue.find(id.delete('#')), :subject => true, :tracker => true)}"}.html_safe
         else
           content.gsub!(/ #(\d+)/,"").html_safe
       end
--- lib/patches/issue_patch.rb
+++ lib/patches/issue_patch.rb
@@ -5,11 +5,22 @@ module IssuePatch
 
       def tree_child(level_arg, subject_arg, task_arg)
         content ||= ""
-        if level_arg >= 0
-           self.children.each_with_index do |child|
-            content += "  <h#{subject_arg}> #{child.subject} #{task_arg.empty? ? "" : "##{child.id}"} </h#{subject_arg}>"
+        if level_arg == 'all' || level_arg > 0
+          self.children.each_with_index do |child|
+            if subject_arg == 'none'
+              content += ""
+            elsif subject_arg.is_a?(Fixnum)
+              case task_arg
+                when 'full'
+                  content += " <h#{subject_arg}> ##{child.id} </h#{subject_arg}>"
+                when 'link'
+                  content += " <h#{subject_arg}> #{child.subject} ##{child.id} </h#{subject_arg}>"
+                else
+                  content += " <h#{subject_arg}> #{child.subject} ##{child.id} </h#{subject_arg}>"
+              end
+            end
             content += RedCloth.new(child.description).to_html + "<br>"
-            content += child.tree_child(level_arg - 1, subject_arg + 1, task_arg)
+            content += child.tree_child(level_arg=='all' ? 'all' : level_arg -1, subject_arg == 'none' ? 'none' : subject_arg + 1, task_arg)
           end
         end
         return content
